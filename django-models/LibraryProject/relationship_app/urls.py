50e62e5b9333131f9479d1b67a280eaebaa4b1d9
Frankline <frankkeybet@gmail.com>
2026-01-16 18:09:43 +0300

    url and views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
+    path('books/', list_books, name='list_books'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]

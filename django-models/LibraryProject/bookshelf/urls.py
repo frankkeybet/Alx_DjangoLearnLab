17bf56cbe7806561eb8be18b9c9dc281423e6c3b
Frankline <frankkeybet@gmail.com>
2026-01-17 19:04:12 +0300

    role based access control

---
+++ django-models/LibraryProject/bookshelf/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views.admin_view import admin_dashboard
+from .views.librarian_view import librarian_dashboard
+from .views.member_view import member_dashboard
+
+urlpatterns = [
+    path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('librarian-dashboard/', librarian_dashboard, name='librarian_dashboard'),
+    path('member-dashboard/', member_dashboard, name='member_dashboard'),
+]

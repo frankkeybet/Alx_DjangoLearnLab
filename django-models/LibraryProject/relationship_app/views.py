1347dbeac229591902336f8c6c68da150c65e262
Frankline <frankkeybet@gmail.com>
2026-01-17 19:40:31 +0300

    Enforce custom book permissions in relationship_app views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -66,18 +66,45 @@ def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
 
-@permission_required('bookshelf.can_add_book')
+
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        publication_year = request.POST.get('publication_year')
+
+        Book.objects.create(
+            title=title,
+            author=author,
+            publication_year=publication_year
+        )
+        return redirect('book_list')
+
     return render(request, 'relationship_app/add_book.html')
 
 
-@permission_required('bookshelf.can_change_book')
-def edit_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.publication_year = request.POST.get('publication_year')
+        book.save()
+        return redirect('book_list')
+
     return render(request, 'relationship_app/edit_book.html', {'book': book})
 
 
-@permission_required('bookshelf.can_delete_book')
-def delete_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+
     return render(request, 'relationship_app/delete_book.html', {'book': book})

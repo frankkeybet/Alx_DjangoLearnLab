9042b025f9ac5529564c9e60fafd3ec397b9d767
frankkeybet <frankkeybet@gmail.com>
2026-01-31 16:14:06 +0300

    implementing permissions and authentication

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.viewsets import ModelViewSet
 #from rest_framework.viewsets import ApiView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
 
 
 
@@ -15,4 +22,13 @@ class BookViewSet(viewsets.ModelViewSet):
     def list(self, request):
         queryset = Book.objects.all()
         serializer_class = BookSerializer
+        #permission_classes = [IsAuthenticated]
+        permission_classes = [IsAuthenticatedOrReadOnly]
+
+
         
+class AdminBookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
+    authentication_classes = [TokenAuthentication]

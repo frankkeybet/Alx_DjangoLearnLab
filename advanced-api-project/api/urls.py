4685344ec61efb8b6701757f02587144864e1b37
frankkeybet <frankkeybet@gmail.com>
2026-02-07 14:45:09 +0300

    customiziing views and urls endpoints

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -14,12 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# advanced_api_project/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from api.views import(BookListView,
+    BookDetailView,
+    BookCreateView,
+    BookUpdateView,
+    BookDeleteView,)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # this handles all Book endpoints
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
-
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions,filters
+from .models import Book
+from .serializers import BookSerializer ,AuthorSerializer
+from rest_framework.permissions import BasePermission
 
 # Create your views here.
+
+class IsAdminUserOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return request.user and request.user.is_staff
+
+
+# List all books (GET)
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # anyone can view
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author']
+
+# Retrieve single book by ID (GET)
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+# Create a new book (POST)
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # only logged in users
+
+    def perform_create(self, serializer):
+        isbn = serializer.validated_data.get('isbn')
+        if len(isbn) != 13:
+            raise serializers.ValidationError("ISBN must be 13 characters")
+        serializer.save()
+
+# Update existing book (PUT/PATCH)
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Delete a book (DELETE)
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+   # permission_classes = [permissions.IsAuthenticated]

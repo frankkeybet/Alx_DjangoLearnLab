49fa1c3b1cdea72afcb12b544c22f1e26e51501e
frankkeybet <frankkeybet@gmail.com>
2026-02-07 14:48:16 +0300

    customiziing views and urls endpoints

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions,filters
 from .models import Book
-from .serializers import BookSerializer ,AuthorSerializer
+from .serializers import BookSerializer 
 from rest_framework.permissions import BasePermission
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 

6f976c70e7f2c70ed0690b420173d32c3238393c
frankkeybet <frankkeybet@gmail.com>
2026-02-12 14:01:08 +0300

    implementing comment functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
 
     # Comments
-    path("post/<int:pk>/comment/new/", CommentCreateView.as_view(), name="comment_new"),
+    path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="comment_new"),
     path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment_update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment_delete"),
 

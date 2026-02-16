a2ca1f0828f7bffcc7c59def8ae813529944e071
frankkeybet <frankkeybet@gmail.com>
2026-02-16 15:06:37 +0300

    refactored the code to use auth.token

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,18 +3,15 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
 
-User = get_user_model()
-
-
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ["username", "email", "password", "bio", "profile_picture"]
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email"),
             password=validated_data["password"],
@@ -23,7 +20,6 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
 
         Token.objects.create(user=user)
-
         return user
 
 

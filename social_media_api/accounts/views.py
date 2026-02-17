5568969947afaed7f48e84e2cb2abe13d0882c27
frankkeybet <frankkeybet@gmail.com>
2026-02-17 18:01:50 +0300

    implimented the follow,unfollow and feed actions with token authentication

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, status,permissions
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny ,IsAuthenticated
 from rest_framework.authtoken.models import Token
-#from rest_framework.decorators import api_view ,permission_classes
+from rest_framework.decorators import api_view ,permission_classes
 
 from .serializers import RegisterSerializer, UserSerializer ,LoginSerializer
 from django.contrib.auth import authenticate

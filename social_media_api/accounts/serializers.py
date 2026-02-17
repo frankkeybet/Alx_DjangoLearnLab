ece770357efed57a6db16197e5bdfe0195a0d6f6
frankkeybet <frankkeybet@gmail.com>
2026-02-17 18:15:31 +0300

    fix checker on get_user

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,17 +1,20 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from .models import CustomUser
+from django.contrib.auth import get_user_model
 
+#from .models import CustomUser
+
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ["id", "username", "email", "password", "bio", "profile_picture"]
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        user = User.get_user_model().objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email"),
             password=validated_data["password"],
@@ -19,7 +22,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             profile_picture=validated_data.get("profile_picture")
         )
 
-        Token.objects.create(user=user)
+        #Token.objects.create(user=user)
         return user
 
 
